748f8d5b291ce739a9518688e9665803eee06174
jontorregano <jontorregano@gmail.com>
2017-05-15 18:59:34 -0500

    Added Basic Order Structure Need to Add DB Connections

---
+++ FoodSiteMVC/FoodSiteMVC/Models/Orders.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FoodSiteMVC.Models
+{
+    public class Orders
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Item { get; set; }
+        public decimal Price { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
